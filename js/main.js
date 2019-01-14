c087699a5a5b14f763f643eecacf48f8d5aba4b7
dj0mlinho <30881187+dj0mlinho@users.noreply.github.com>
2019-01-14 23:51:03 +0100

    3. commit - finished template for index.html

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
-$(".back-to-top").click(function () {
-  $("html, body").animate({scrollTop: 0}, 1000);
+$.ajax({
+  url: "https://raw.githubusercontent.com/Danilovesovic/shop/master/shop.json",
+  dataType: "json"
+}).done(function(res) {
+  let dataBase = res;
+  //CREATE HTML
+  let text = "";
+  let template = $('[type="template"]').html();
+  for (let i = 0; i < dataBase.length; i++) {
+    text += template
+      .replace(/{{imgSrc}}/gi, dataBase[i].imgSrc)
+      .replace(/{{productTitle}}/gi, dataBase[i].productTitle)
+      .replace(/{{price}}/gi, dataBase[i].price)
+      .replace(/{{model}}/gi, dataBase[i].model);
+  }
+  $("#insertTemplate").html(text);
+});
+
+//BOJANOV FRONT
+$(".back-to-top").click(function() {
+  $("html, body").animate(
+    {
+      scrollTop: 0
+    },
+    1000
+  );
 });
